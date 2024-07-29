7cde876995135e94f6fb4afa428feac60fead3cd
SajeedAninda <sajaninda@gmail.com>
2024-07-29 19:29:56 +0600

    Linked border countries

--- src/Components/CountryDetails/Details.jsx
+++ src/Components/CountryDetails/Details.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import "./details.css"
 import leftArrow from "../../assets/leftArrow.svg"
 
 const Details = () => {
+    const [allCountries, setAllCountries] = useState([]);
     let [countryDetail] = useLoaderData();
+
+    useEffect(() => {
+        const fetchAllCountries = async () => {
+            const response = await fetch('https://restcountries.com/v3.1/all');
+            const data = await response.json();
+            setAllCountries(data);
+        };
+        fetchAllCountries();
+    }, []);
 
     const formatPopulation = (population) => {
         return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
+    const getCountryNameByCca3 = (cca3) => {
+        const country = allCountries.find(country => country.cca3 === cca3);
+        return country ? country.name.common : cca3;
     };
 
     return (
@@ -42,7 +57,7 @@ const Details = () => {
                                 </p>
 
                                 <p className='text-[15px]'>
-                                    <span className='text-[15px] font-semibold'>Sub Region: </span>{countryDetail?.capital?.[0]}
+                                    <span className='text-[15px] font-semibold'>Capital: </span>{countryDetail?.capital?.[0]}
                                 </p>
                             </div>
 
@@ -64,14 +79,24 @@ const Details = () => {
                                         ? Object.values(countryDetail.languages).join(', ')
                                         : 'Language information not available'}
                                 </p>
-
-
                             </div>
                         </div>
+
+                        <p className='text-[13px] mt-16 flex items-center gap-2'>
+                            <span className='text-[15px] font-semibold'>Border Countries: </span>
+                            {countryDetail.borders?.length ? (
+                                countryDetail.borders.map((borderCountry, i) => (
+                                    <Link className='shadow-lg mr-2 px-1 py-2 hover:shadow-xl' key={i} to={`/${getCountryNameByCca3(borderCountry)}`}>
+                                        {getCountryNameByCca3(borderCountry)}
+                                    </Link>
+                                ))
+                            ) : (
+                                <span>No border countries</span>
+                            )}
+                        </p>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
